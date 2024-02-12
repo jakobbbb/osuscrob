189c3ae075588b8e413d8cbdced55febb91a7f6a
Jakob Ruckel <jakob@ruckel.de>
2024-02-12 21:12:20 +0100

    Refactoring uwu

--- osuscrob/osuscrob.py
+++ osuscrob/osuscrob.py
@@ -19,18 +19,38 @@ CONFIG = {
     "lastfm_pass": "",
 }
 
-SCRIPT_DIR = os.path.realpath(os.path.dirname(__file__))
-CONFIG_PATH = os.path.join(SCRIPT_DIR, "config.yml")
-
 
 class OsuScrob:
+    def __init__(self):
+        self.dir = os.path.expanduser("~/.config/osuscrob")
+        if not os.path.exists(self.dir):
+            os.mkdir(self.dir)
+
+        self.prev_scrobs_path = os.path.join(self.dir, "prev_scrobs.lst")
+        self.config_path = os.path.join(self.dir, "config.yml")
+
+        self.cfg = self.load_config()
+        if not self.check_config(self.cfg):
+            raise Exception(
+                (f"Please fix the config ({self.config_path}) and re-run :>")
+            )
+
+        self.prev_scrobs = []
+        if os.path.exists(self.prev_scrobs_path):
+            with open(self.prev_scrobs_path, "r", encoding="utf-8") as f:
+                self.prev_scrobs = f.read().splitlines()
+
+        assert self.cfg
+        self.init_lastfm_api(**self.cfg)
+        self.init_osu_api(**self.cfg)
+
     def load_config(self):
-        if not os.path.exists(CONFIG_PATH):
+        if not os.path.exists(self.config_path):
             self.write_config(CONFIG)
-            print(f"Please configure in {CONFIG_PATH} :)")
+            print(f"Please configure in {self.config_path} :)")
             return None
         else:
-            with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+            with open(self.config_path, "r", encoding="utf-8") as f:
                 return yaml.safe_load(f.read())
 
     def check_config(self, cfg):
@@ -55,8 +75,35 @@ class OsuScrob:
         return True
 
     def write_config(self, cfg):
-        with open(CONFIG_PATH, "w+", encoding="utf-8") as f:
+        with open(self.config_path, "w+", encoding="utf-8") as f:
             f.write(yaml.dump(cfg, sort_keys=True))
+
+    def init_lastfm_api(
+        self,
+        lastfm_api_key,
+        lastfm_shared_secret,
+        lastfm_user,
+        lastfm_pass,
+        **_,
+    ):
+        assert self.cfg
+
+        self.lastfm = pylast.LastFMNetwork(
+            api_key=lastfm_api_key,
+            api_secret=lastfm_shared_secret,
+            username=lastfm_user,
+            password_hash=pylast.md5(lastfm_pass),
+        )
+
+    def init_osu_api(self, osu_client_id, osu_client_secret, **_):
+        assert self.cfg
+
+        domain = Domain.LAZER if self.cfg["osu_lazer"] else Domain.OSU
+        self.osu = Ossapi(
+            osu_client_id,
+            osu_client_secret,
+            domain=domain,
+        )
 
     def get_recent(self, api, osu_user, **_) -> List[Score]:
         user_id = api.user(osu_user).id
@@ -73,37 +120,12 @@ class OsuScrob:
         return r.sub("", title)
 
     def main(self):
-        cfg = self.load_config()
-        if not self.check_config(cfg):
-            print(f"Please fix the config ({CONFIG_PATH}) and re-run :>")
-            return 1
+        assert self.cfg
 
-        script_dir = os.path.realpath(os.path.dirname(__file__))
-        prev_scrobs_path = os.path.join(script_dir, "prev_scrobs.lst")
-        prev_scrobs = []
-        if os.path.exists(prev_scrobs_path):
-            with open(prev_scrobs_path, "r", encoding="utf-8") as f:
-                prev_scrobs = f.read().splitlines()
-
-        if cfg is None:
-            return 1
-
-        lastfm = pylast.LastFMNetwork(
-            api_key=cfg["lastfm_api_key"],
-            api_secret=cfg["lastfm_shared_secret"],
-            username=cfg["lastfm_user"],
-            password_hash=pylast.md5(cfg["lastfm_pass"]),
-        )
-
-        domain = Domain.LAZER if cfg["osu_lazer"] else Domain.OSU
-        api = Ossapi(
-            cfg["osu_client_id"], cfg["osu_client_secret"], domain=domain
-        )
-
-        recent = self.get_recent(api, **cfg)
+        recent = self.get_recent(self.osu, **self.cfg)
         scrobbles = []
         for play in recent:
-            if play.rank == Grade.F or str(play) in prev_scrobs:
+            if play.rank == Grade.F or str(play) in self.prev_scrobs:
                 continue
             if play.beatmapset is None:
                 continue
@@ -117,17 +139,17 @@ class OsuScrob:
                 "artist": artist,
                 "timestamp": timestamp,
             }
-            if str(scrobble) not in prev_scrobs:
+            if str(scrobble) not in self.prev_scrobs:
                 scrobbles.append(scrobble)
-                prev_scrobs.append(str(scrobble))
-                prev_scrobs.append(str(play))
+                self.prev_scrobs.append(str(scrobble))
+                self.prev_scrobs.append(str(play))
                 print(scrobble)
 
         print(f"Scrobbling {len(scrobbles)} tracks!")
-        lastfm.scrobble_many(scrobbles)
+        self.lastfm.scrobble_many(scrobbles)
 
-        with open(prev_scrobs_path, "w+", encoding="utf-8") as f:
-            f.write("\n".join(prev_scrobs))
+        with open(self.prev_scrobs_path, "w+", encoding="utf-8") as f:
+            f.write("\n".join(self.prev_scrobs))
 
 
 def main():
